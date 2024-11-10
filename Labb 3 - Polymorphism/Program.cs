cc93c5aadd2844fdc51e6b0a339b79c72248b7fd
Conix <Conix@DESKTOP-CNLH10G>
2024-11-10 19:00:48 +0100

    Final changes try catch

--- Labb 3 - Polymorphism/Program.cs
+++ Labb 3 - Polymorphism/Program.cs
@@ -12,77 +12,109 @@ namespace Labb_3___Polymorphism
         static void Main(string[] args)                     // Leon Johansson SUT24
         {
             Console.WriteLine("Hello World!");
-
+            // Instances of diffrent shapes
             Geometry circle = new Circle(4);
             Geometry square = new Square(7);
             Geometry rectangle = new Rectangle(2, 8);
             Geometry triangel = new Triangle(7, 9);
-
+            // List that contains the diffrent Geometry types
             List<Geometry> list = new List<Geometry> { circle, square, rectangle, triangel};
 
-            foreach (Geometry g in list) 
+            foreach (Geometry g in list) // Looping through each shape in the list and thier own Area methohd
             {
-                Console.WriteLine($"{ g.GetType().Name} har arean: {g.Area() }");
-                Console.ReadKey();
+                Console.WriteLine("");
+                // Console.WriteLine($"{ g.GetType().Name} har arean: {g.Area() }");
+                if (g == circle) 
+                {
+                    Console.WriteLine($"En cirkel med radeien 4 är: {g.Area()}");
+                }
+                else if (g == square) 
+                {
+                    Console.WriteLine($"En fyrkant med sidorna 7 är: {g.Area()}");
+                }
+                else if (g == rectangle) 
+                {
+                    Console.WriteLine($"En rektangel med höjden 2 och bredden 8 är: {g.Area()}");
+                }
+                else if (g == triangel) 
+                {
+                    Console.WriteLine($"En triangel med höjden 7 och bredden 9 är: {g.Area()}");
+                }
             }
-            Console.WriteLine(" Vill du testa själv? välj en av de 4 formerna som du vill se deras area: " +
+            // Meny if the user wanna try for themself
+            Console.WriteLine("\nVill du testa själv? välj en av de 4 formerna som du vill se deras area: " +
                 "\n 1. Cirkel" +
                 "\n 2, Fyrkant" +
                 "\n 3. Rektangel" +
                 "\n 4. Triangel" +
                 "\n 5. Exit");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            
-            switch(choice)
+
+            try
             {
-                case 1:
-                    Console.WriteLine("Skriv in radien på cirklen:");
-                    double userRadius = Convert.ToDouble(Console.ReadLine());
-                    Geometry userCircle = new Circle(userRadius);
-                    Console.WriteLine($"{ userCircle.Area() }");
-                    Console.ReadKey();
-                    break;
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1: // If the user wanna get area of circle
+                        Console.WriteLine("Skriv in radien på cirklen:");
+                        double userRadius = Convert.ToDouble(Console.ReadLine());
+                        Geometry userCircle = new Circle(userRadius);
+                        Console.WriteLine($"{userCircle.Area()}");
+                        Console.ReadKey();
+                        Console.WriteLine("Tack för mig ha en bra dag");
+                        Console.ReadKey();
+                        break;
 
-                case 2:
-                    Console.WriteLine("Skriv in sidan på fyrkanten:");
-                    double userSide = Convert.ToDouble(Console.ReadLine());
-                    Geometry userSquare = new Square(userSide);
-                    Console.WriteLine($"{ userSquare.Area() }");
-                    break;
+                    case 2: // " " of square
+                        Console.WriteLine("Skriv in sidan på fyrkanten:");
+                        double userSide = Convert.ToDouble(Console.ReadLine());
+                        Geometry userSquare = new Square(userSide);
+                        Console.WriteLine($"{userSquare.Area()}");
+                        Console.WriteLine("Tack för mig ha en bra dag");
+                        Console.ReadKey();
+                        break;
 
-                case 3:
-                    Console.WriteLine("Skriv in höjden av rektanglen:");
-                    double userHeight = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Skriv in breden av rektanglen:");
-                    double userWidth = Convert.ToDouble(Console.ReadLine());
-                    Geometry userRectangle = new Rectangle(userHeight, userWidth);
-                    Console.WriteLine($"{ userRectangle.Area() }");
+                    case 3: // " "of rectangle
+                        Console.WriteLine("Skriv in höjden av rektanglen:");
+                        double userHeight = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Skriv in breden av rektanglen:");
+                        double userWidth = Convert.ToDouble(Console.ReadLine());
+                        Geometry userRectangle = new Rectangle(userHeight, userWidth);
+                        Console.WriteLine($"{userRectangle.Area()}");
+                        Console.WriteLine("Tack för mig ha en bra dag");
+                        Console.ReadKey();
+                        break;
 
-                    break;
-
-                case 4:
-                    Console.WriteLine("Skriv in höjden på trianglen:");
-                    double userTriHeight = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Skriv in basen på trianglen:");
-                    double userTriWidth =Convert.ToDouble(Console.ReadLine());
-                    Geometry userTriangle = new Triangle(userTriHeight, userTriWidth);
-                    Console.WriteLine($"{ userTriangle.Area() }");
-                    break;
-                case 5:
-
-                    break;
-                default:
-                    Console.WriteLine("En siffra dipshit");
-                    break;
+                    case 4: // " " of triangle
+                        Console.WriteLine("Skriv in höjden på trianglen:");
+                        double userTriHeight = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Skriv in basen på trianglen:");
+                        double userTriWidth = Convert.ToDouble(Console.ReadLine());
+                        Geometry userTriangle = new Triangle(userTriHeight, userTriWidth);
+                        Console.WriteLine($"{userTriangle.Area()}");
+                        Console.WriteLine("Tack för mig ha en bra dag");
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        // Exits program
+                        break;
+                    default:
+                        Console.WriteLine("En siffra ditt stolpskott");
+                        break;
+                }
+            }
+            catch (Exception ex)    // Catch if user is silly goose
+            {
+                Console.WriteLine("Oväntat fel eller fel inmatning, programmet stängs ner");
+                Console.ReadKey();
             }
         }
     } 
-    public abstract class Geometry 
+    public abstract class Geometry  // Geometyr abstract class 
     {
-        public abstract double Area(); 
+        public abstract double Area();  // Abstarct method for calculating area for the subclasses
         
     }
-    public class Circle : Geometry 
+    public class Circle : Geometry // Subclass of Geometry
     {
         public double Radius { get; set; }
 
@@ -90,12 +122,12 @@ namespace Labb_3___Polymorphism
         {
             Radius = radius;
         }
-        public override double Area()
+        public override double Area()   // Override method for Area specific for a circle
         { 
             return Radius * Radius * Math.PI;
         }
     }
-    public class Rectangle : Geometry 
+    public class Rectangle : Geometry   // Subclass of Geometry
     {
         public double Height { get; set; }
         public double Width { get; set; }
@@ -105,12 +137,12 @@ namespace Labb_3___Polymorphism
             Width = width;
         }
 
-        public override double Area() 
+        public override double Area() // Override method for Area specific for a rectangle
         {
             return Height * Width;
         }
     }
-    public class Square : Geometry 
+    public class Square : Geometry  // Subclass of Geometry
     {
         public double Side { get; set; }
         public Square(double side)
@@ -118,13 +150,13 @@ namespace Labb_3___Polymorphism
             Side = side;
         }
 
-        public override double Area()
+        public override double Area()   // Override method for Area specific for a square
         {
             return Side * Side;
         }
   
     }
-    public class Triangle : Geometry
+    public class Triangle : Geometry    // Subclass of Geometry
     {
         public double Height { get; set; }
         public double Width { get; set; }
@@ -134,7 +166,7 @@ namespace Labb_3___Polymorphism
             Width = width;
         }
 
-        public override double Area()
+        public override double Area()   // Override method for Area specific for a triangle
         {
             return Height * Width / 2;
         }
